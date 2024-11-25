e362df556265afee9336158ee3bbe84dbdcec45c
Rapid2k <162550224+Rapid2k@users.noreply.github.com>
2024-11-25 09:11:49 +0545

    meal surve

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,19 @@
                 <!-- search Box -->
                 <div>
                     <form class="max-w-sm mx-auto">
-    <label for="countries" class=" block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-    <select 
+    <label for="meals" class=" block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+    <select
+
     v-model="query " 
   
     @change="getProduct (query)"  
     
   
     class=   "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full 
-     p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-      <option selected>Chicken</option>
-      <option :value="category.strCategory" v-for="(category,index) in cate.categories" :key="index">{{ category.strCategory }}</option>
-  
+      dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      
+      <option :value="category.strCategory" v-for="(category,index) in cate.categories" :key="index"> {{ category.strCategory }}</option>
+  <option selected value="">choose a meal</option>
     </select>
   </form>
 
